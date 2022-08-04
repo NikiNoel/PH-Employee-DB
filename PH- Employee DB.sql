f8d5d131543c9933ed608f5fc984100dce7cf140
Niki <NikiNoel1218@gmail.com>
2022-08-04 15:26:16 -0400

    Completed through Deliverable 2

--- PH- Employee DB.sql
+++ PH- Employee DB.sql
@@ -1,4 +1,5 @@
 -- Queries PH- Employee DB.sql
+-- Deliverable 1:
 
 SELECT e.emp_no, e.first_name, e.last_name, t.title, t.from_date, t.to_date
 INTO retirement_titles
@@ -27,3 +28,24 @@ INTO Retiring_Titles
 FROM unique_titles AS ut
 GROUP BY ut.title
 ORDER BY Count DESC
+
+
+-- Deliverable 2:
+-- Create Mentorship Eligibility table as mentorship_eligibilty.csv
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+e.first_name,
+e.last_name,
+e.birth_date,
+de.from_date,
+de.to_date,
+t.title
+INTO mentorship_eligibilty
+FROM employees AS e
+INNER JOIN dept_emp AS de
+	ON (e.emp_no = de.emp_no)
+INNER JOIN titles AS t
+	ON (e.emp_no = t.emp_no)
+--filter on to_date to get current employees ('9999-01-01')
+WHERE de.to_date = '9999-01-01'
+AND e.birth_date BETWEEN '1965-01-01' AND '1965-12-31'
+ORDER BY e.emp_no;
